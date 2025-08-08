21802bbbb93f6ed01ce903dc237008260f89f5cf
Petro <petrofradi1994@gmail.com>
2025-08-08 11:29:44 -0300

    criado arquivo de extracao e ajustado arquivo principal

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
-import pandas as pd 
-import numpy as np 
+
 import streamlit as st 
+from src.extraction import load_data
 
-def load_data():
-    return pd.read.csv('C:\Users\petro\Documents\Petro\repos\Curso_git\projeto\projeto\data\processed\bikes_completed.csv')
-
+st.set_page_config(layout='wide')
 
 def main():
     df = load_data()
